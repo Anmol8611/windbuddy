c1ae4848622daeed5afa77953223390f07da27d4
Anmol8611 <kumaranmol8611@gmail.com>
2023-06-20 13:03:03 +0530

    initiated an weather app 'windbuddy' project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hey , this is windbuddy</h1>
+      <p>just initiated this weather app project</p>
+    </>
+  )
+}
+
+export default App
